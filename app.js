49d0a7ebaf66125a5b9d468428a8688eb820a4d8
prelator <patrick@fairusetube.org>
2014-02-07 17:31:19 -0700

    final commit

--- app.js
+++ app.js
@@ -1,61 +1,67 @@
-//============== Global functions ===========
-if (!String.prototype.supplant) {
-    String.prototype.supplant = function (o) {
-        return this.replace(
-            /\{([^{}]*)\}/g,
-            function (a, b) {
-                var r = o[b];
-                return typeof r === 'string' || typeof r === 'number' ? r : a;
-            }
-        );
-    };
-}
-
-var wishlist = []; 
-
 //Document ready
 $(document).ready(function(){
     
-    //============ Click handlers ==============
-    
-   $(document).on('click', '#add-item', function() {
-        $(document).find('.wish-list-item').remove();
-        var name = $(this).closest('.product').data('name');
-        var price = $(this).closest('.product').data('price');
+    //============== Global functions ===========
+    if (!String.prototype.supplant) {
+        String.prototype.supplant = function (o) {
+            return this.replace(
+                /\{([^{}]*)\}/g,
+                function (a, b) {
+                    var r = o[b];
+                    return typeof r === 'string' || typeof r === 'number' ? r : a;
+                }
+            );
+        };
+    }
+
+    var checkDup = function(name){
         var isDup = false;
-        var newItem = {
-            title: name,
-            price: price,
-            included: true
-            }; 
         for (var i = 0; i < wishlist.length; i++) {
             if (wishlist[i].title === name) {
                 isDup = true;
-                wishlist[i].included = true;
+            }       
+        }
+        return isDup;        
+    };        
+
+    //Global variables
+    var wishlist = []; 
+    var wishTemplate = $('#wish-template').html();
+    var wishCompile = Handlebars.compile(wishTemplate);
+
+    var productTemplate = $('#product-template').html();
+    var productCompile = Handlebars.compile(productTemplate);
+
+    //============ Click handlers ==============
+    
+   //Add Button
+   $(document).on('click', '#add-item', function() {
+        $(document).find('.wish-list-item').remove();
+        var name = $(this).closest('.product').attr('data-name');
+        var price = $(this).closest('.product').attr('data-price');
+        var newItem = {
+            title: name,
+            price: price,
+            };         
+        if (!checkDup(name)) {
+            wishlist.push(newItem);
             }
-        }
-        if (!isDup) {
-        wishlist.push(newItem);
-        }
         renderWishlist();
     });
 
+   //Remove button
    $(document).on('click', '.btn-remove', function() {
-      var name = $(this).closest('.wish-item').data('name');
+      var name = $(this).closest('.wish-item').attr('data-name');
       for (var i = 0; i < wishlist.length; i++) {
         if (wishlist[i].title === name) {
-            wishlist[i].included = false;
+            wishlist.splice(i, 1);
             }    
         }
       $(this).closest('.wish-list-item').remove();
-      getIncluded();
-      console.dir(listIncluded);
    });
 
     //============ Handlebars Generation ==============
     var renderProducts = function(){
-        var productTemplate = $('#product-template').html();
-        var productCompile = Handlebars.compile(productTemplate);
         for (var i = 0; i < productsData.productsList.length; i++) {
             var newProduct = productCompile(productsData.productsList[i]);
             $('#product-container').append(newProduct);
@@ -63,20 +69,10 @@ $(document).ready(function(){
     };
 
     var renderWishlist = function(){
-        var wishTemplate = $('#wish-template').html();
-        var wishCompile = Handlebars.compile(wishTemplate);
-        getIncluded();
-        console.dir(listIncluded);
-        for (var i = 0; i < listIncluded.length; i++) {
-            var newWish = wishCompile(listIncluded[i]);
+        for (var i = 0; i < wishlist.length; i++) {
+            var newWish = wishCompile(wishlist[i]);
             $('#wishlist').append(newWish);            
         }
-    };
-
-    var getIncluded = function(){
-        listIncluded = wishlist.filter(function(item){
-            return item.included;
-            });
     };
 
     //=========== Functions called on document ready =============
